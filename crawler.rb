dc2b604c6dcd2b5f8ffc2747f539556fe1d5d096
minamijoyo <minamijoyo@gmail.com>
2015-01-23 20:09:31 +0900

    Change output result from stdout to the file

--- crawler.rb
+++ crawler.rb
@@ -1,35 +1,48 @@
 require 'octokit'
 
-# GitHub API access token must be exported
-access_token = ENV['GITHUBAPI_ACCESS_TOKEN']
-raise "GITHUBAPI_ACCESS_TOKEN must be exported !!" if access_token.nil?
+puts "Start initialization"
+# Open file for results
+File.open("commits.txt", "w") do |file|
 
-# Octokit initialization
-client = Octokit::Client.new(:access_token => access_token)
-raise "Octokit initialization error" if client.nil?
+  # GitHub API access token must be exported
+  access_token = ENV['GITHUBAPI_ACCESS_TOKEN']
+  raise "GITHUBAPI_ACCESS_TOKEN must be exported !!" if access_token.nil?
 
-# Octokit setting
-client.auto_paginate = true
+  # Octokit initialization
+  client = Octokit::Client.new(:access_token => access_token)
+  raise "Octokit initialization error" if client.nil?
 
-# check the current rate limit before call API
-sleep 60 unless client.rate_limit.remaining
+  # Octokit setting
+  client.auto_paginate = true
 
-# search popular repositories
-search_results = client.search_repos('stars:>10000', :per_page => 100)
-repos = search_results.items.map(&:full_name)
+  # print login user
+  puts "Login user: #{client.user.login}"
 
-# for each repository
-repos.each do |repo|
   # check the current rate limit before call API
+  puts "Check rate limit: #{client.rate_limit}"
   sleep 60 unless client.rate_limit.remaining
 
-  # get commits list on the repository
-  commits = client.commits(repo, :per_page => 100)
+  # search popular repositories
+  puts "Search repositories on GitHub"
+  search_results = client.search_repos('stars:>10000', :per_page => 100)
+  repos = search_results.items.map(&:full_name)
+  puts "Target repositories: #{repos}"
 
-  # for each commit
-  commits.each do |commit|
-    sha = commit[:sha]
-    message = commit[:commit][:message]
-    puts "#{repo}, #{sha}, #{message}"
+  # for each repository
+  repos.each do |repo|
+    # check the current rate limit before call API
+    puts "Check rate limit: #{client.rate_limit}"
+    sleep 60 unless client.rate_limit.remaining
+
+    # get commits list on the repository
+    puts "Get commit messages on github:#{repo}"
+    commits = client.commits(repo, :per_page => 100)
+
+    # for each commit
+    commits.each do |commit|
+      sha = commit[:sha]
+      message = commit[:commit][:message]
+      file.puts "#{repo}, #{sha}, #{message}"
+    end
   end
 end
