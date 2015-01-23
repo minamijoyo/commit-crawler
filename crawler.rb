a58389426e52499950524ac733893671a3bbad7f
minamijoyo <minamijoyo@gmail.com>
2015-01-23 19:24:39 +0900

    Implement the enumaration of papular repositories

--- crawler.rb
+++ crawler.rb
@@ -1,13 +1,35 @@
 require 'octokit'
 
-client = Octokit::Client.new(:access_token => ENV['GITHUBAPI_ACCESS_TOKEN'])
+# GitHub API access token must be exported
+access_token = ENV['GITHUBAPI_ACCESS_TOKEN']
+raise "GITHUBAPI_ACCESS_TOKEN must be exported !!" if access_token.nil?
+
+# Octokit initialization
+client = Octokit::Client.new(:access_token => access_token)
+raise "Octokit initialization error" if client.nil?
+
+# Octokit setting
 client.auto_paginate = true
 
-repo = client.repo 'minamijoyo/sample_app'
-commits = client.commits repo.full_name
+# check the current rate limit before call API
+sleep 60 unless client.rate_limit.remaining
 
-commits.each do |commit|
-  sha = commit[:sha]
-  message = commit[:commit][:message]
-  puts "#{sha}, #{message}"
+# search popular repositories
+search_results = client.search_repos('stars:>10000', :per_page => 100)
+repos = search_results.items.map(&:full_name)
+
+# for each repository
+repos.each do |repo|
+  # check the current rate limit before call API
+  sleep 60 unless client.rate_limit.remaining
+
+  # get commits list on the repository
+  commits = client.commits(repo, :per_page => 100)
+
+  # for each commit
+  commits.each do |commit|
+    sha = commit[:sha]
+    message = commit[:commit][:message]
+    puts "#{repo}, #{sha}, #{message}"
+  end
 end
