9e7e3b7a41bc4bf513bdcd305f8dab1e30e529b1
minamijoyo <minamijoyo@gmail.com>
2015-01-24 23:43:36 +0900

    Delete unnesessary code

--- crawler.rb
+++ crawler.rb
@@ -17,14 +17,6 @@ client.auto_paginate = true
 # print login user
 puts "Login user: #{client.user.login}"
 
-# check the current rate limit before call API
-puts "Check rate limit: #{client.rate_limit}"
-until client.rate_limit.remaining do
-  reset_in = client.rate_limit.resets_in
-  puts "rate limit sleep in #{reset_in}"
-  sleep reset_in
-end
-
 # search popular repositories
 puts "Search repositories on GitHub"
 search_results = client.search_repos('stars:>10000', :per_page => 100)
